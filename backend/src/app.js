f499bbecc614c5eb8a058efa038de530cdd757d4
elifnurkarakoc <elifnurkarakoc@gmail.com>
2021-10-24 21:12:06 +0300

    :white_check_mark: [backend] add products crud operations

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,12 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const indexRouter = require('./routes/index');
+const productsRouter = require('./routes/products');
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,22 +19,26 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/products', productsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json(err);
 });
 
+if (!module.parent) {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server is up on ${PORT} Port`));
+}
 module.exports = app;
--- backend/src/routes/products.js
+++ backend/src/routes/products.js
@@ -1,9 +1,92 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const { nanoid } = require('nanoid');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const router = express.Router();
+
+const products = [
+  {
+    id: '1',
+    name: 'Product1',
+    image:'imageurl',
+    brand:'brand',
+    color:'black',
+    price:1000,
+    createdDate:new Date(),
+    discountPercentage:30,
+  },
+  {
+    id: '2',
+    name: 'Product2',
+    image:'imageurl',
+    brand:'brand',
+    color:'black',
+    price:1000,
+    createdDate:new Date(),
+    discountPercentage:30,
+  },
+  {
+    id: '3',
+    name: 'Product3',
+    image:'imageurl',
+    brand:'brand',
+    color:'black',
+    price:1000,
+    createdDate:new Date(),
+    discountPercentage:30,
+  },
+];
+
+router.get('/', (req, res) => {
+  res.json(products);
+});
+
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+  const product = products.find((item) => item.id === id);
+
+  if (!product) {
+    return next({ message: 'product not found' });
+  }
+
+  res.json(product);
+});
+
+router.post('/', (req, res) => {
+  const data = req.body;
+  const product = { id: nanoid(), ...data };
+
+  products.push(product);
+  res.json(product);
+});
+
+router.put('/:id', (req, res, next) => {
+  const { id } = req.params;
+  const index = products.findIndex((item) => item.id === id);
+
+  if (index < 0) {
+    return next({ message: 'Product not found!' });
+  }
+
+  const data = req.body;
+  const product = products[index];
+  const updated_data = { ...product, ...data };
+  products[index] = updated_data;
+
+  res.json(products[index]);
+});
+
+router.delete('/:id', (req, res, next) => {
+  const { id } = req.params;
+  const index = products.findIndex((item) => item.id === id);
+
+  if (index < 0) {
+    return next({ message: 'Product not found!' });
+  }
+
+  const deleted_product = products[index];
+  products.splice(index, 1);
+
+  res.json(deleted_product);
 });
 
 module.exports = router;
