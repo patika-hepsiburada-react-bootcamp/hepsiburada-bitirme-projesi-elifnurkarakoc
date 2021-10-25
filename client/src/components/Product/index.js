ed02c28fa548b112dfa2321a1b906b8e31ee8b89
elifnurkarakoc <elifnurkarakoc@gmail.com>
2021-10-25 17:46:27 +0300

    :lipstick: [client]  add product cart button and passive button css

--- client/src/components/Product/index.js
+++ client/src/components/Product/index.js
@@ -1,36 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./index.module.css";
 const Product = ({ name, image, brand, color, price, discountPercentage }) => {
+  const [hover, setHover] = useState(false);
   const discountPrice = (value) =>
     (value - (value * discountPercentage) / 100).toFixed(2);
 
   return (
-    <div className={styles.product}>
-      <div className={styles.product_image}>
-        <img src={image} alt="" />
+    <div
+      className={styles.product}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <div className={styles.product_container}>
+        <div className={styles.product_image}>
+          <img src={image} alt="" />
+        </div>
+        <div className={styles.title}>{name}</div>
+        {!hover && (
+          <>
+            {" "}
+            <div className={styles.filter}>
+              <div className={styles.info}>
+                <p>
+                  <b>Marka: </b>
+                  {brand}
+                </p>
+              </div>
+              <div className={styles.info}>
+                <p>
+                  <b>Renk: </b>
+                  {color}
+                </p>
+              </div>
+            </div>
+            <div className={styles.price}>{discountPrice(price)} TL</div>
+            {discountPercentage !== 0 && (
+              <div className={styles.no_discount_price}>
+                <span className={styles.line}> {price} TL</span>{" "}
+                <span className={styles.red}>{discountPercentage}%</span>
+              </div>
+            )}
+          </>
+        )}
+        {hover && (
+          //TODO: add  basket control
+          // <>
+          //   <button className={styles.add_cart_button}>Sepete Ekle</button>
+          // </>
+          <>
+            <button className={styles.passive_button}>
+              Bu ürünü sepete ekleyemezsiniz.
+            </button>
+          </>
+        )}
       </div>
-      <div className={styles.title}>{name}</div>
-      <div className={styles.filter}>
-        <div className={styles.info}>
-          <p>
-            <b>Marka: </b>
-            {brand}
-          </p>
-        </div>
-        <div className={styles.info}>
-          <p>
-            <b>Renk: </b>
-            {color}
-          </p>
-        </div>
-      </div>
-      <div className={styles.price}>{discountPrice(price)} TL</div>
-      {discountPercentage !== 0 && (
-        <div className={styles.no_discount_price}>
-          <span className={styles.line}> {price} TL</span>{" "}
-          <span className={styles.red}>{discountPercentage}%</span>
-        </div>
-      )}
     </div>
   );
 };
