f7b0196f5bfa073ce7eac2ffda82b6bc9da82b87
elifnurkarakoc <elifnurkarakoc@gmail.com>
2021-10-25 16:42:33 +0300

    :lipstick: [client]  add product module css

--- client/src/components/Product/index.js
+++ client/src/components/Product/index.js
@@ -1,25 +1,36 @@
 import React from "react";
+import styles from "./index.module.css";
+const Product = ({ name, image, brand, color, price, discountPercentage }) => {
+  const discountPrice = (value) =>
+    (value - (value * discountPercentage) / 100).toFixed(2);
 
-const Product = ({
-  id,
-  name,
-  image,
-  brand,
-  color,
-  price,
-  createdDate,
-  discountPercentage,
-}) => {
   return (
-    <div>
-      {id}
-      {name}
-      {image}
-      {brand}
-      {color}
-      {price}
-      {createdDate}
-      {discountPercentage}
+    <div className={styles.product}>
+      <div className={styles.product_image}>
+        <img src={image} alt="" />
+      </div>
+      <div className={styles.title}>{name}</div>
+      <div className={styles.filter}>
+        <div className={styles.info}>
+          <p>
+            <b>Marka: </b>
+            {brand}
+          </p>
+        </div>
+        <div className={styles.info}>
+          <p>
+            <b>Renk: </b>
+            {color}
+          </p>
+        </div>
+      </div>
+      <div className={styles.price}>{discountPrice(price)} TL</div>
+      {discountPercentage !== 0 && (
+        <div className={styles.no_discount_price}>
+          <span className={styles.line}> {price} TL</span>{" "}
+          <span className={styles.red}>{discountPercentage}%</span>
+        </div>
+      )}
     </div>
   );
 };
