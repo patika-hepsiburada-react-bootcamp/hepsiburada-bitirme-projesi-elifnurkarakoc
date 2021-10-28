8e63d4d9f3d2666a6aff629d69869fb7426579e0
elifnurkarakoc <elifnurkarakoc@gmail.com>
2021-10-28 11:42:09 +0300

    :white_check_mark: [backend] add redis

--- backend/src/routes/products.js
+++ backend/src/routes/products.js
@@ -1,25 +1,37 @@
-const express = require('express');
-const { nanoid } = require('nanoid');
-const { generateProducts } = require('./generate');
+const express = require("express");
+const { nanoid } = require("nanoid");
+const { generateProducts } = require("./generate");
 // const products = require("./constant");
 const router = express.Router();
-const products=generateProducts();
-router.get('/', (req, res) => {
+//const products=generateProducts();
+const { cache } = require("./redis-service");
+
+var products;
+
+const setProducts = async function redisProducts() {
+  console.log("go to redis");
+  var value = await cache();
+  products = JSON.parse(JSON.stringify(value));
+  console.log(products);
+};
+setProducts();
+
+router.get("/", (req, res) => {
   res.json(products);
 });
 
-router.get('/:id', (req, res, next) => {
+router.get("/:id", (req, res, next) => {
   const { id } = req.params;
   const product = products.find((item) => item.id === id);
 
   if (!product) {
-    return next({ message: 'product not found' });
+    return next({ message: "product not found" });
   }
 
   res.json(product);
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   const data = req.body;
   const product = { id: nanoid(), ...data };
 
@@ -27,12 +39,12 @@ router.post('/', (req, res) => {
   res.json(product);
 });
 
-router.put('/:id', (req, res, next) => {
+router.put("/:id", (req, res, next) => {
   const { id } = req.params;
   const index = products.findIndex((item) => item.id === id);
 
   if (index < 0) {
-    return next({ message: 'Product not found!' });
+    return next({ message: "Product not found!" });
   }
 
   const data = req.body;
@@ -43,12 +55,12 @@ router.put('/:id', (req, res, next) => {
   res.json(products[index]);
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete("/:id", (req, res, next) => {
   const { id } = req.params;
   const index = products.findIndex((item) => item.id === id);
 
   if (index < 0) {
-    return next({ message: 'Product not found!' });
+    return next({ message: "Product not found!" });
   }
 
   const deleted_product = products[index];
---
+++ backend/src/routes/redis-service.js
@@ -0,0 +1,25 @@
+const redisClient = require("./redis-client");
+const { generateProducts } = require("./generate");
+
+async function cache() {
+  var result = await redisClient.getAsync("products");
+  result = (result !== "undefined" && JSON.parse(result)) || null;
+  console.log("result", result);
+  if (!result) {
+    var value = generateProducts();
+    setRedisProduct(value);
+    console.log("value", value);
+    return value;
+  }
+
+  return result;
+}
+
+async function setRedisProduct(products) {
+  await redisClient.setAsync("products", JSON.stringify(products));
+}
+
+module.exports = {
+  cache,
+  setRedisProduct,
+};
