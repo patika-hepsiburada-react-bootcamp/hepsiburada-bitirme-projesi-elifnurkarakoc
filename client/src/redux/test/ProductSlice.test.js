ccd5e6ca2404553e1fc97763b3bd2a8e4336c9c4
elifnurkarakoc <elifnurkarakoc@gmail.com>
2021-10-29 16:54:43 +0300

    :white_check_mark: [client] update productslice test

--- client/src/redux/test/ProductSlice.test.js
+++ client/src/redux/test/ProductSlice.test.js
@@ -2,7 +2,10 @@ import { store } from "redux/store";
 import {
   searchProducts,
   updateProducts,
-  updateFilter,
+  // updateFilter,
+  updateSelectedFilterKey,
+  updateSort,
+  updateIndexs,
 } from "redux/slices/ProductSlice";
 
 const sampleFilteredProducts = [
@@ -31,52 +34,83 @@ const sampleFilteredProducts = [
     createdDate: "2020-03-05T16:01:38.049Z",
   },
 ];
+describe("ProductSlice", () => {
+  test("searchProducts", () => {
+    let state = store.getState().products;
+    const initialVisibleItems = state.visibleItems;
 
-test("searchProducts", () => {
-  let state = store.getState().products;
-  const initialVisibleItems = state.visibleItems;
+    let searchInput = "h";
+    store.dispatch(searchProducts(searchInput));
+    state = store.getState().products;
+    let newlyVisibleItems = state.visibleItems;
+    expect(newlyVisibleItems).toEqual(initialVisibleItems);
 
-  let searchInput = "a";
-  store.dispatch(searchProducts(searchInput));
-  state = store.getState().products;
-  let newlyVisibleItems = state.visibleItems;
-  expect(newlyVisibleItems).toEqual(initialVisibleItems);
+    searchInput = "hi";
+    store.dispatch(searchProducts(searchInput));
+    state = store.getState().products;
+    newlyVisibleItems = state.visibleItems;
+    expect(newlyVisibleItems).toHaveLength(0);
+  });
 
-  searchInput = "aa";
-  store.dispatch(searchProducts(searchInput));
-  state = store.getState().products;
-  newlyVisibleItems = state.visibleItems;
-  expect(newlyVisibleItems).toHaveLength(0);
+  test("updateProducts", () => {
+    let state = store.getState().products;
+    const initialVisibleItems = state.visibleItems;
+    store.dispatch(updateProducts(sampleFilteredProducts));
+    state = store.getState().products;
+    let newlyVisibleItems = state.visibleItems;
+    expect(newlyVisibleItems).not.toEqual(initialVisibleItems);
+  });
+
+  //test("updateFilter", () => {
+    //   let state = store.getState().products;
+    //   const initialColorFilters = state.colorFilters;
+    //   const initialBrandFilters = state.brandFilters;
+    //   store.dispatch(updateFilter(sampleFilteredProducts));
+    //   state = store.getState().products;
+    //   const newlyColorFilters = state.colorFilters;
+    //   const newlyBrandFilters = state.brandFilters;
+    //   console.log({newlyBrandFilters});
+    // //   expect(newlyColorFilters).toEqual({ Siyah: 2 });
+    // //   expect(newlyBrandFilters).toEqual({ Apple: 1, Xiamio: 1 });
+    //   expect(newlyColorFilters).toEqual({});
+    //   expect(newlyBrandFilters).toEqual({});
+    //   // expect(newlyColorFilters).not.toEqual(initialColorFilters);
+    //   // expect(newlyBrandFilters).not.toEqual(initialBrandFilters);
+ // });
+  var selectedKeys = [
+    ["color", "Siyah"],
+    ["brand", "Xiamio"],
+  ];
+  test("updateSelectedFilterKey", () => {
+    let state = store.getState().products;
+    const initialSelectedFilter = state.selectedFilter;
+    store.dispatch(updateSelectedFilterKey(selectedKeys));
+    state = store.getState().products;
+    const newlySelectedFilter = state.selectedFilter;
+    expect(newlySelectedFilter).not.toEqual(initialSelectedFilter);
+  });
+
+  test("updateSort", () => {
+    var sort = "price_desc";
+    let state = store.getState().products;
+    const initialSort = state.sort;
+    store.dispatch(updateSort(sort));
+    state = store.getState().products;
+    const newlySort = state.sort;
+    expect(newlySort).not.toEqual(initialSort);
+  });
+
+  test("updateIndexs", () => {
+    let state = store.getState().products;
+    const initialFirstIndex = state.indexOfFirstProducts;
+    const initialLastIndex = state.indexOfLastProducts;
+    store.dispatch(
+      updateIndexs({ indexOfFirstProducts: 12, indexOfLastProducts: 24 })
+    );
+    state = store.getState().products;
+    const newlyFirstIndex = state.indexOfFirstProducts;
+    const newlyLastIndex = state.indexOfLastProducts;
+    expect(newlyFirstIndex).not.toEqual(initialFirstIndex);
+    expect(newlyLastIndex).not.toEqual(initialLastIndex);
+  });
 });
-
-test("updateProducts", () => {
-  let state = store.getState().products;
-  const initialVisibleItems = state.visibleItems;
-  store.dispatch(updateProducts(sampleFilteredProducts));
-  state = store.getState().products;
-  let newlyVisibleItems = state.visibleItems;
-  expect(newlyVisibleItems).not.toEqual(initialVisibleItems);
-});
-
-test("updateFilter", () => {
-  let state = store.getState().products;
-  const initialColorFilters = state.colorFilters;
-  const initialBrandFilters = state.brandFilters;
-  store.dispatch(updateFilter(sampleFilteredProducts));
-  state = store.getState().products;
-  const newlyColorFilters = state.colorFilters;
-  const newlyBrandFilters = state.brandFilters;
-//   expect(newlyColorFilters).toEqual({ Siyah: 2 });
-//   expect(newlyBrandFilters).toEqual({ Apple: 1, Xiamio: 1 });
-  expect(newlyColorFilters).toEqual({});
-  expect(newlyBrandFilters).toEqual({});
-  expect(newlyColorFilters).not.toEqual(initialColorFilters);
-  expect(newlyBrandFilters).not.toEqual(initialBrandFilters);
-
-});
-
-// test("updateSelectedFilterKey", () => {});
-
-// test("updateSort", () => {});
-
-// test("getCurrentProducts", () => {});
