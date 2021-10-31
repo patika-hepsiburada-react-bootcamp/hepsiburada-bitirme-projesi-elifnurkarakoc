71f12fcf019a9597a5015ec31a28ecd262ba52f9
elifnurkarakoc <elifnurkarakoc@gmail.com>
2021-10-31 12:57:38 +0300

    :white_check_mark: [backend] add redis event log

--- backend/src/routes/redis-client.js
+++ backend/src/routes/redis-client.js
@@ -4,7 +4,10 @@ const client = redis.createClient({
   port: process.env.REDIS_PORT,
   host: process.env.REDIS_HOST,
 });
-
+client.on('connect', () => console.log('Connected to Redis 🔫'));
+client.on('ready', () => console.log('Connected to Redis and ready to use... ♥️'));
+client.on('error', (err) => console.log(err.message));
+client.on('end', () => console.log('Client disconnected from Redis'));
 module.exports = {
   ...client,
   getAsync: promisify(client.get).bind(client),
